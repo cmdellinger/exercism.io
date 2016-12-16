a1a07cdba9efe2424c05edc02b0d6e9ce24c0687
cmdellinger <cdellinger@Charless-MacBook-Pro.local>
2016-12-16 15:01:40 -0500

    Refactor - stripped code down to pass tests only
    stripped code down to barebones of what needs to be there to pass the
    tests

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -9,81 +9,46 @@ Create a 24 hour Clock class that can add or subract minutes
 """
 
 ## ------
-## import modules
-## ------
-
-# none
-
-## ------
-## function definitions
-## ------
-
-# none
-
-## ------
-## class definitions
+## Esxercism.io solution
 ## ------
 
 class Clock:
     """ create a 24 hour clock that can add or subract minutes """
 
-    # private functions
-    def __clock_time(self, time = []): # -> Int [hours, minutes]
-        """ expects list [hours, minutes] and returns 24 hr clock [hours, minutes] """
-        # added vars for readability
-        hours = time[0]
-        minutes = time[1]
-        
+    # init and call behavior
+    def __init__(self, hours, minutes):
+        """ create list of hours and minutes changed to 24 hour time"""
         set_hr = (hours + int(minutes/60)) % 24
         if set_hr < 0:
             set_hr += 24
-
         set_min = minutes % 60
         if set_min < 0:
             set_min += 60
-
-        return [set_hr, set_min]
-
-    def __clock_str(self, time = []): # -> String XX:XX
-        """ takes time as [hours, minutes] and returns string XX:XX """
-        # added vars for readability
-        hours = time[0]
-        minutes = time[1]
-        
-        return str(hours).rjust(2, '0') + ":" + str(minutes).rjust(2, '0')
-    
-    # init and call behavior
-    def __init__(self, hours, minutes):
-        """ create list of hours and minutes and change to 24 hour time"""
-        self.time = [hours, minutes]
-        self.time = self.__clock_time(self.time)
+        self.time = [set_hr, set_min]
 
     def __str__(self):
-        return self.__clock_str(self.time)
+        """ returns string of 24 clock time in XX:XX format """
+        return str(self.time[0]).rjust(2, '0') + ":" + str(self.time[1]).rjust(2, '0')
 
     def __eq__(self, other):
-        """ compare 24 hour clock time """
+        """ equal comparison of 24 hour clock time """
         return self.time == other.time
 
     def __ne__(self, other):
-        """ compare 24 hours clock time """
+        """ not equal comparison of 24 hours clock time """
         return self.time != other.time
 
     # methods
-    def add(self, minutes): # -> String (XX:XX)
-        """ add minutes to time and return new 24 hour clock string in form XX:XX """
-        self.time[1] += minutes
-        self.time = self.__clock_time(self.time)
-        return self.__clock_str(self.time)
+    def add(self, minutes): # -> Clock
+        """ returns clock with additional minutes """
+        return Clock(self.time[0], self.time[1] + minutes)
     
-    def subtract(self, minutes): # -> String (XX:XX)
-        """ subtract minutes from time and return new 24 hour clock string in form XX:XX """
-        self.time[1] -= minutes
-        self.time = self.__clock_time(self.time)
-        return self.__clock_str(self.time)
+    def subtract(self, minutes): # -> Clock
+        """ returns clock minus minutes using add()"""
+        add(-minutes)
 
 ## ------
-## main
+## Command-line implementation
 ## ------
 
 if __name__ == '__main__':
