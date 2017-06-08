50c38ec8cfde06e315c060e02673eac905a6fe44
Charles M Dellinger <charles.dellinger@gmail.com>
2017-06-08 14:39:47 -0400

    Exercism.io Python difference-of-squares solution
    Exercism.io Python difference-of-squares solution

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,38 @@
-def square_of_sum():
-    pass
+"""
+Exercism.io assignment - difference_of_squares.py
+Written by cmdellinger
+    
+Usage:
+    None
+    
+Find the sum of the squares, the square of the sum, or the difference between the two.
+"""
 
+## ------
+## Exercism.io solution
+## ------
 
-def sum_of_squares():
-    pass
+def square_of_sum(number = 0): # -> integer
+    ''' returns the square of the sums of natural numbers up to input number '''
+    sum = 0
+    for integer in xrange(1, number + 1):
+        sum += integer
+    return sum**2
 
+def sum_of_squares(number = 0): # -> integer
+    ''' returns the sum of the squares of natural numbers up to input number '''
+    sum = 0
+    for integer in xrange(1, number + 1):
+        sum += integer**2
+    return sum
 
-def difference():
-    pass
+def difference(number = 0): # -> integer
+    ''' returns the difference betwenn the sum of squares and the square of the sum '''
+    return abs(sum_of_squares(number) - square_of_sum(number))
+
+## ------
+## Command-line implementation
+## ------
+
+if __name__ == '__main__':
+    print(__doc__)
