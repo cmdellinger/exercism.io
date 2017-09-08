b3bbb7d51594c69693138dda688ae4c2ed8019e7
Charles M Dellinger <charles.dellinger@gmail.com>
2017-09-08 12:11:29 -0400

    exercism.io Ruby - Hello World
    exercise file for hello_world in Ruby from exercism.io

---
+++ ruby/hello-world/hello_world.rb
@@ -0,0 +1,11 @@
+#### hello_world ####
+#
+#   this script creates a HelloWorld class with class
+#   method hello
+#
+
+class HelloWorld
+    def self.hello(name="World")
+        "Hello, #{name}!"
+    end
+end
