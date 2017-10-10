da8e0d32b0339e0a9cb7b2fd7c6003afeb3e0b8b
Charles M Dellinger <charles.dellinger@gmail.com>
2017-10-09 23:22:36 -0400

    Ruby raindrops exercise solution

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,46 @@
+#### raindrops ####
+#
+#   Complement class with class method convert()
+#       convert() takes in an integer and returns a string
+#       of raindrops. The string contains a concatenation of
+#       "Pling", "Plang", "Plong" if the integer has factors
+#       3, 5, 7 respectively. If the integer has none of those
+#       factors, the integer is returned as a string.
+#
+#   Raindrops.convert(<input_number>)
+#
+#   Parameters:
+#       <input_number> : integer
+#           any integer to be converted into "raindrops"
+#
+#   Returns:
+#       <raindrops> : string
+#           a concatenation of "Pling", "Plang", and "Plong"
+#           for factors of 3, 5, 7 respectively. if no relevant
+#           factors, then passed number is returned as a string.
+
+class Raindrops
+  @@Raindrop_table = {3=> 'Pling',
+                      5=> 'Plang',
+                      7=> 'Plong'}
+
+  def self.convert(input_number = 0) #-> ""
+      # check for relevant factors
+      factors = @@Raindrop_table.keys.find_all {|integer| input_number%integer == 0}
+      # return passed number if no relevant factors
+      return input_number.to_s if factors.length == 0
+      # build raindrops
+      raindrops = ""
+      factors.each {|factor| raindrops << @@Raindrop_table[factor]}
+      raindrops
+  end
+end
+
+#### BookKeeping Version ####
+#
+#   the version of the tests that are being run
+#
+
+module BookKeeping
+  VERSION = 3 # Where the version number matches the one in the test.
+end
