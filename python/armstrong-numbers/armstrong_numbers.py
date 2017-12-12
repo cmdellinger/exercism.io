2742f42291665185b88dc643e9ace70b2f6eacc6
Charles M Dellinger <charles.dellinger@gmail.com>
2017-12-12 12:12:12 -0500

    command line implementation for Python armstrong_numbers
    added command line implementation to Python armstrong_numbers solution using docopt

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -4,7 +4,9 @@ written by (GitHub):    cmdellinger
 Python version:         2.7
     
 Usage:
-    
+    armsrtong_numbers.py <number>...
+
+Determines whether a number is an armstrong number.
 
 An Armstrong number is a number that is the sum of its own digits each
 raised to the power of the number of digits.
@@ -12,9 +14,24 @@ raised to the power of the number of digits.
 See README file for more detailed information.
 """
 
+## ------
+## Exercism.io solution
+## ------
+
 def is_armstrong(number = 0): #-> boolean
     ''' returns whether a number is an Armstrong number '''
     # convert to string, split by putting in list, then changing values to int
     digits = map(int, list(str(number)))
     # is sum of nth power digits (x_1^n + x_2^n...) is equal to original number
     return number == reduce(lambda x,y: x+y, map(lambda x: x**len(digits), digits))
+
+## ------
+## Command-line implementation
+## ------
+
+if __name__ == '__main__':
+    from docopt import docopt
+    # get list of numbers; list has numbers as strings so map to int
+    numbers = map(int, docopt(__doc__)['<number>'])
+    for number in numbers:
+        print "{} is {}an armstrong number".format(number, 'not '*(not is_armstrong(number)))
