a2724909b23655bdda4bcec08616546d780e1e63
Charles M Dellinger <charles.dellinger@gmail.com>
2017-12-12 09:05:28 -0500

    functional solution to allergies in Python
    used Python 2.7

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,3 +1,21 @@
 class Allergies(object):
-    def __init__(self):
-        pass
+    ALLERGY_LIST = ['eggs',
+                    'peanuts',
+                    'shellfish',
+                    'strawberries',
+                    'tomatoes',
+                    'chocolate',
+                    'pollen',
+                    'cats']
+                    
+    def build_lst(self):
+        bin_to_s = '{0:b}'.format(self.number)[::-1][:8]
+        allergy_list = [self.ALLERGY_LIST[index] for index, char in enumerate(bin_to_s) if int(char)]
+        return allergy_list
+
+    def __init__(self, number = 0):
+        self.number = number
+        self.lst = self.build_lst()
+
+    def is_allergic_to(self, allergy = ''):
+        return allergy in self.lst
