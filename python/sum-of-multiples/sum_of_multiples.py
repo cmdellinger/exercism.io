226b6aae4080d7ec2a471a04e440b3d400137224
Charles M Dellinger <charles.dellinger@gmail.com>
2017-12-15 11:45:26 -0500

    Python sum_of_multiples solution

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,28 @@
+"""
+Exercism.io assignment: sum_of_multiples.py
+written by (GitHub):    cmdellinger
+Python version:         2.7
+
+See README for problem constraints.
+"""
+
+## ------
+## Exercism.io solution
+## ------
+
+def sum_of_multiples(limit = 0, multiples = []): #-> int
+    ''' sums all integers in [0, limit) that are divisible by any of the given
+        multiples.
+    '''
+    factors = []
+    for multiple in multiples:
+        factors += range(multiple, limit, multiple)
+    return sum(set(factors))
+
+'''
+# slower solution by about x10
 def sum_of_multiples(limit, multiples):
-    pass
+    def divisible(number):
+        return any([not number % multiple for multiple in multiples])
+    return sum([multiple for multiple in range(limit) if divisible(multiple)])
+'''
