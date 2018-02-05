b1e3c0538d36dc363ecb932e94d117a6c83556d1
Charles M Dellinger <charles.dellinger@gmail.com>
2018-02-05 12:12:37 -0500

    solution to Python reverse string

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,2 +1,12 @@
+"""
+Exercism.io assignment: reverse_string.py
+written by (GitHub):    cmdellinger
+Python version:         2.7
+    
+Reverse a string.
+    
+See README for additional problem constraints.
+"""
 def reverse(input=''):
-    pass
+    """ Reverses the given string. """
+    return input[::-1]
